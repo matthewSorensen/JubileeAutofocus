e927f1b6fb551d791123f6169c27b7a0110b9198
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2020-07-04 14:13:47 -0700

    Make that increasingly complex

--- sync_test.py
+++ sync_test.py
@@ -3,16 +3,12 @@ import json
 import subprocess
 import time
 
+import asyncio
+
 POLL_INTERVAL_S = 0.1
 SOCKET_ADDRESS = '/var/run/dsf/dcs.sock'
 MM_BUFFER_SIZE = 65536
 
-
-def sendblob(sock, blob):
-    sock.sendall(json.dumps(blob).encode())
-
-def getblob(sock, size):
-    return json.loads(sock.recv(MM_BUFFER_SIZE).decode())
 
 def dict_or_list_iter(obj):
     """ Returns a key/value iterator for a dict or a list """
@@ -66,25 +62,96 @@ def partial_update(state, template, patch):
         else:
             partial_update(state, t, v)
 
+def keys_in_template(template):
+    """ Traverse a template and return all keys that will eventually be used. """
+    if isinstance(template, str):
+        yield template
+    else:
+        it = dict_or_list_iter(template)
+        if it:
+            for k,v in it:
+                yield from keys_in_template(v)
+
+def initialize_partial_state(state, template):
+    """ Build an empty state object with slots for all of the values we're tracking,
+    and adjust the template to make sure to track the machine status. """
+
+    for k in keys_in_template(template):
+        state[k] = None
+    idle_name = 'idle'
+    while idle_name in state:
+        idle_name += "_"
+    # Then make sure the idle status is in the template
+    if 'state' in template:
+        if 'status' in template['state']:
+            # It's already in the state, so use the original name
+            idle_name = template['state']['status']
+        else:
+            state[idle_name] = None
+            template['state']['status'] = idle_name
+    else:
+        state[idle_name] = None
+        template['state'] = {'status' : idle_name}
+        
+    return template, idle_name
+ 
+
+async def sendblob(writer,obj):
+    writer.write(json.dumps(obj).encode())
+    await writer.drain()
+
+async def getblob(reader):
+    data = await reader.read(MM_BUFFER_SIZE)
+    return json.loads(data.decode())
+                  
+    
+async def track_machine_state(state, template = None, idle_event = None):
+    
+    status, idle_key  = None, None
+    if template:
+        template, idle_key = initialize_partial_state(state, template)
+    
+    reader, writer = await asyncio.open_unix_connection(SOCKET_ADDRESS)
+    await sendblob(writer, {"mode":"subscribe","version": 8, "subscriptionMode": "Patch"})
+
+    
+    message = await getblob(reader)
+
+    if template:  
+        partial_update(state, template, message)
+        status = state[idle_key]
+    else:
+        recursive_update(state, message)
+        status = state['state']['status']
+
+    if idle_event:
+        if status == 'idle':
+            idle_event.set()
+        else:
+            idle_event.clear()
+
+    while True:
+
+        await sendblob(writer,{"command" : "Acknowledge"})
             
-template = {'heat' : {'heaters': {0 : {'current' : 'bed_temp'}}}, 'state' : {'uptime' : 'uptime', 'status' : 'status'}}
+        message = await getblob(reader)
+        new_status = None
 
-state = dict()
+        if template:
+            partial_update(state, template, message)
+            new_status = state[idle_key]
+        else:
+            recursive_update(state, message)
+            new_status = state['state']['status']
 
-subscribe_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-subscribe_socket.connect(SOCKET_ADDRESS)
-subscribe_socket.setblocking(True)
+        if new_status == 'idle' and idle_event:
+            idle_event.set() # Wake everyone up!
 
+        status = new_status
+        
 
-sendblob(subscribe_socket, {"mode":"subscribe","version": 8, "subscriptionMode": "Patch"})
+    
+template = {'heat' : {'heaters': {0 : {'current' : 'bed_temp'}}}, 'state' : {'status' : 'idle'}}
 
+asyncio.run(track_machine_state(dict(), template = template))
 
-partial_update(state, template, getblob(subscribe_socket, MM_BUFFER_SIZE))
-
-while True:
-
-    sendblob(subscribe_socket, {"command" : "Acknowledge"})
-    blob = getblob(subscribe_socket, MM_BUFFER_SIZE)
-
-    partial_update(state, template, blob)
-    print(state)
